1f1d608aa7418cfcbb50943d3b4a2c7b684707bd
SoLuutions <vynchsuan@gmail.com>
2025-08-13 02:38:14 +0800

    text edits

--- app.js
+++ app.js
@@ -284,7 +284,6 @@ function initPricingModule() {
     bvaAppTerms.innerHTML = `
       <ul>
         <li>Pay one-time fee and save nearly $70 compared to monthly billing</li>
-        <li>Continued access after the first year requires an 18% annual maintenance fee</li>
         <li>Cancel any time in the first 30 days</li>
       </ul>
     `;
@@ -295,7 +294,6 @@ function initPricingModule() {
         bvaWorkbooksTerms.innerHTML = `
           <ul>
             <li>Pay one-time fee and save nearly $70 compared to monthly billing</li>
-            <li>Continued access after the first year requires an 18% annual maintenance fee</li>
             <li>Cancel any time in the first 30 days</li>
           </ul>
         `;
